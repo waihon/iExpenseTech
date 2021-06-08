e6fd7749fc541c232345afb528fef38bc8703bc6
Waihon Yew <yewwaihon@gmail.com>
2021-06-08 23:41:34 +0800

    Use @State for data that is local to the current view

--- iExpenseTech/ContentView.swift
+++ iExpenseTech/ContentView.swift
@@ -7,10 +7,22 @@
 
 import SwiftUI
 
+struct User {
+  var firstName = "Bilbo"
+  var lastName = "Baggins"
+}
+
 struct ContentView: View {
+  @State private var user = User()
+  
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    VStack {
+      Text("Your name is \(user.firstName) \(user.lastName)")
+      TextField("First name", text: $user.firstName)
+        .padding(10)
+      TextField("Last name", text: $user.lastName)
+        .padding(10)
+    }
   }
 }
 
