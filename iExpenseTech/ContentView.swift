d8e2f4e74158ab3ba175734ef02432b9373df70c
Waihon Yew <yewwaihon@gmail.com>
2021-06-11 23:55:44 +0800

    Archive and unarchive custom type that conforms to Codable protocol

--- iExpenseTech/ContentView.swift
+++ iExpenseTech/ContentView.swift
@@ -7,7 +7,38 @@
 
 import SwiftUI
 
+struct User: Codable {
+  var firstName: String
+  var lastName: String
+}
+
 struct ContentView: View {
+  @State private var user = User(firstName: "Taylor", lastName: "Swift")
+  @State private var fullName = ""
+  
+  var body: some View {
+    VStack {
+      Text("\(fullName)")
+      Button("Save User") {
+        let encoder = JSONEncoder()
+        
+        if let data = try? encoder.encode(self.user) {
+          UserDefaults.standard.set(data, forKey: "UserData")
+        }
+      }
+    }
+    .onAppear() {
+      if let savedUser = UserDefaults.standard.object(forKey: "UserData") as? Data {
+        let decoder = JSONDecoder()
+        if let loadedUser = try? decoder.decode(User.self, from: savedUser) {
+          fullName = loadedUser.firstName + " " + loadedUser.lastName
+        }
+      }
+    }
+  }
+}
+
+struct UserDefaultsiew: View {
   @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
   
   var body: some View {
