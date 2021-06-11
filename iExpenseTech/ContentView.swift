33e6881f2c2c2ea52348806835dc44a1c13f3b9b
Waihon Yew <yewwaihon@gmail.com>
2021-06-11 22:34:45 +0800

    Add an Edit/Done button to the navigation bar that lets users delete several rows more easily.

--- iExpenseTech/ContentView.swift
+++ iExpenseTech/ContentView.swift
@@ -12,18 +12,21 @@ struct ContentView: View {
   @State private var currentNumber = 1
 
   var body: some View {
-    VStack {
-      List {
-        ForEach(numbers, id: \.self) {
-          Text("\($0)")
+    NavigationView {
+      VStack {
+        List {
+          ForEach(numbers, id: \.self) {
+            Text("\($0)")
+          }
+          .onDelete(perform: removeRows)
         }
-        .onDelete(perform: removeRows)
+        
+        Button("Add Number") {
+          self.numbers.append(self.currentNumber)
+          self.currentNumber += 1
+        }
       }
-      
-      Button("Add Number") {
-        self.numbers.append(self.currentNumber)
-        self.currentNumber += 1
-      }
+      .navigationBarItems(leading: EditButton())
     }
   }
   
