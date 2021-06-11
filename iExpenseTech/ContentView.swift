53b96de73e159d95acb4179614bc2fef52ac75b3
Waihon Yew <yewwaihon@gmail.com>
2021-06-11 23:12:43 +0800

    Write data to and read data from local UserDefaults

--- iExpenseTech/ContentView.swift
+++ iExpenseTech/ContentView.swift
@@ -8,6 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+  
+  var body: some View {
+    Button("Tap count: \(tapCount)") {
+      self.tapCount += 1
+      UserDefaults.standard.set(self.tapCount, forKey: "Tap")
+    }
+  }
+}
+
+struct OnDeleteView: View {
   @State private var numbers = [Int]()
   @State private var currentNumber = 1
 
